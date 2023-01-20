1228f7a8698478c7f6b742bc6e43f43bde7e2f62
Ambar Javier Bermudez <ajavier210@gmail.com>
2023-01-19 21:17:52 -0500

    Save the Universe

--- HW/Wk6/Save the Universe/index.js
+++ HW/Wk6/Save the Universe/index.js
@@ -1,29 +1,28 @@
 class Ship {
     constructor(name, hull, firepower, accuracy) {
-        this.name = name,
-        this.hull = hull,
-        this.firepower = firepower, 
-        this.accuracy = accuracy
+        this.name = name;
+        this.hull = hull;
+        this.firepower = firepower; 
+        this.accuracy = accuracy;
     }
 
 }
 
-class MainShip extends Ship {
+class MainShip extends Ship { 
     constructor(name, hull, firepower, accuracy) {
         super(name, hull, firepower, accuracy)
-
-            this.name = "USS Java",
-                this.hull = 20,
-                this.firepower = 5,
+            this.name = "USS Assembly"
+                this.hull = 20
+                this.firepower = 5
                 this.accuracy = .7
         }
        attackAlien(target){
         let randomNum = Math.random()
         if (randomNum <= this.accuracy){
             target.hull -= this.firepower 
-              window.alert(`Attacked ${target.name} for ${this.firepower} damage!`)
+              window.alert(`Attacked ${target.allAlien} for ${this.firepower} Damage!`)
         } else{
-           (window.alert('You missed us!'))
+           (window.alert('You missed!')) ////the .random is working. Idk Why it loads mid game though///
         }
         target.attackMain(this)
        }
@@ -33,7 +32,7 @@ class MainShip extends Ship {
   class AlienShip extends Ship {
     constructor(name, hull, firepower, accuracy){
        super(name, hull, firepower, accuracy)
-       this.name = name,
+       this.name = name;
        this.hull = (Math.floor(Math.random() * 4) + 3), // function getRandomArbitrary(min, max) { Math.random() * (max - min) + min;
        this.firepower = Math.floor((Math.random() * 3) + 2), // Math.floor(math.random() * ((max - min) + 1) + min) <== whole number
        this.accuracy =  ((Math.random()* .2) + .6).toFixed(2) // (Math.random() * (max - min)) + min <== decimal
@@ -41,17 +40,18 @@ class MainShip extends Ship {
    attackMain(target) {
        let randomNum = Math.random()
         if (randomNum < this.accuracy) {
-           target.hull -= this.firepower
-           window.alert(`Attacked ${target.name} for ${this.firepower} damage!`); // target is the actual target of the attack. any changes made on target.whatever are going to affect the actual thing given to it.
+           target.hull -= this.firepower;
+           window.alert(`Attacked ${target.name} for ${this.firepower} damage!`); 
 
         }
-        else { window.alert('You have missed!') }
+        else { window.alert('You missed!') }
+        target.attackAlien(this) /// now no background but 'attacks' working ///
        }
  }
 
 
       // add additional properties for the "Main Ship"'
-    const USSJava = new MainShip 
+    const USSAssembly = new MainShip 
     const alienShip1 = new AlienShip ("Alien Ship 1")
     const alienShip2 = new AlienShip ("Alien Ship 2")
     const alienShip3 = new AlienShip ("Alien Ship 3")
@@ -64,10 +64,21 @@ class MainShip extends Ship {
       console.log(allAlien)
 
 
-  USSJava.attackAlien(alienShip4)
+  USSAssembly.attackAlien(alienShip5) ///when I use allAlien nothing happens///
 
 
-  const attackButton = document.getElementById('attack')
-  attackButton.addEventListener('click', () => {
+  const attackButton = document.getElementById('Attack')
+  attackButton.addEventListener('click', () => { ////I know there is a method missing in the ()///
 
   })
+
+  const retreatButton = document.getElementById('Retreat')
+  retreatButton.addEventListener('click', () => {
+
+  })
+
+  const quitButton = document.getElementById('Quit')
+  quitButton.addEventListener('click', () => {
+
+  })
+  ///once i added target.attackAlien there was no end to the attcks and no background//
