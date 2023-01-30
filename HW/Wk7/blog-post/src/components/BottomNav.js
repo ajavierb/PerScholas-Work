54080c0aa6ede4b430b59487120f43655db3982d
Ambar Javier Bermudez <ajavier210@gmail.com>
2023-01-30 01:30:53 -0500

    Blog Post React updates

---
+++ HW/Wk7/blog-post/src/components/BottomNav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function BottomNav() {
+    return (
+        <div>
+         <nav menu ="menu" aria-label="Main Navigation" role="navigation"> 
+        <ul class="nav2">
+            <li><a href="">Home</a></li>
+            <li><a href="">Women's</a></li>
+            <li><a href="">Men's</a></li>
+            <li><a href="">On The Street</a></li>
+            <li><a href="">The Catwalk</a></li>
+            <li><a href="">AdWatch</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Tips</a></li>
+        </ul>
+        </nav>   
+        </div>
+    )
+}
+export default BottomNav
---
+++ HW/Wk7/blog-post/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BottomNav from './BottomNav'
+
+function Footer() {
+    return (
+        <div>
+            <BottomNav/>
+            <h2> &copy; 2013 Valet Industries Inc.</h2>
+        </div>
+
+    )
+}
+export default Footer
