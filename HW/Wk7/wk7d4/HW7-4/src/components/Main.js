56ca10fd1482a0cf0260d95eeef82dfd39577d87
ajavierb <ajavier210@gmail.com>
2023-01-30 01:40:11 -0500

    Add files via upload

---
+++ HW/Wk7/wk7d4/HW7-4/src/components/Main.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import img1 from './images/blog-image-1.jpg'
+import img2 from './images/blog-image-2.jpg'
+
+
+function Main() {
+    return(
+        <div>
+            <main>
+            <h3 class="h3s">11/12/20</h3>
+          <h2>On the Street in Brooklyn</h2>
+          <img src={img1} alt=" Blog-image"/>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora cum eligendi ullam ut, ipsa
+             doloremque officiis debitis eos pariatur perferendis consequuntur atque id molestiae, dolor suscipit 
+             corporis labore natus Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in 
+             reprehenderit ex velit sint reiciendis. Labore quidem perspiciatis eveniet velit recusandae eos maxime perferendis? 
+             Eius optio ut autem amet atque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab natus, 
+             necessitatibus deserunt nam blanditiis assumenda! Eum maiores architecto, voluptates obcaecati tenetur 
+             recusandae, magnam atque optio, labore quidem eos dolorum fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora cum eligendi ullam ut, ipsa
+             doloremque officiis debitis eos pariatur perferendis consequuntur atque id molestiae, dolor suscipit 
+             corporis labore natus Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in 
+             reprehenderit ex velit sint reiciendis. Labore quidem perspiciatis eveniet velit recusandae eos maxime perferendis? 
+             Eius optio ut autem amet atque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab natus, 
+             necessitatibus deserunt nam blanditiis assumenda! Eum maiores architecto, voluptates obcaecati tenetur 
+             recusandae, magnam atque optio, labore quidem eos dolorum fugiat!
+          </p>
+          <h3 class="tbd"> Continues...</h3>
+          
+
+          <hr/>
+          <article>
+    <h3 class="h3s">11/11/20</h3>
+    <h2>Vintage in Vogue</h2>
+    <img src={img2} alt=" Blog-image"/>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora cum eligendi ullam ut, ipsa
+         doloremque officiis debitis eos pariatur perferendis consequuntur atque id molestiae, dolor suscipit 
+         corporis labore natus Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in 
+         reprehenderit ex velit sint reiciendis. Labore quidem perspiciatis eveniet velit recusandae eos maxime perferendis? 
+         Eius optio ut autem amet atque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab natus, 
+         necessitatibus deserunt nam blanditiis assumenda! Eum maiores architecto, voluptates obcaecati tenetur 
+         recusandae, magnam atque optio, labore quidem eos dolorum fugiat!Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora cum eligendi ullam ut, ipsa
+         doloremque officiis debitis eos pariatur perferendis consequuntur atque id molestiae, dolor suscipit 
+         corporis labore natus Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in 
+         reprehenderit ex velit sint reiciendis. Labore quidem perspiciatis eveniet velit recusandae eos maxime perferendis? 
+         Eius optio ut autem amet atque? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab natus, 
+         necessitatibus deserunt nam blanditiis assumenda! Eum maiores architecto, voluptates obcaecati tenetur 
+         recusandae, magnam atque optio, labore quidem eos dolorum fugiat!
+      </p>
+      <h3 class="tbd">Continues...</h3>
+    </article>
+            </main>
+        </div>
+    )
+}
+export default Main
---
+++ HW/Wk7/wk7d4/HW7-4/src/components/TopNav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TopNav() {
+    return (
+        <div>
+          <nav aria-label="Main Navigation" role="navigation"> 
+            <ul class="nav1">
+                <li><a href="">Women's</a></li>
+                <li><a href="">Men's</a></li>
+                <li><a href="">On The Street</a></li>
+                <li><a href="">The Catwalk</a></li>
+                <li><a href="">AdWatch</a></li>
+                <li><a href="">About</a></li>
+            </ul>
+            </nav>   
+        </div>
+    )
+}
+export default TopNav
