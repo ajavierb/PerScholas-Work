dd8521ce479ad823587a87a1634c26bc443eae50
Ambar Javier Bermudez <ajavier210@gmail.com>
2023-02-13 12:45:49 -0500

    Recipe App

--- HW/Wk10/recipe-app/src/main.js
+++ HW/Wk10/recipe-app/src/main.js
@@ -1,8 +1,6 @@
 
 axios.get(
-    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
-    search +
-    "&app_id=ca8a7842&app_key=8467fdd6590ea00b728798aacaf54866"
+    `https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=ca8a7842&app_key=8467fdd6590ea00b728798aacaf54866`
 )
 
-export default main
+export default MediaMetadataInit;
