2a385ae13117dd9de14f59bad37703e300af3a7e
Ambar Javier Bermudez <ajavier210@gmail.com>
2023-02-15 10:01:40 -0500

    last updates

--- HW/Wk10/recipe-app/src/App.js
+++ HW/Wk10/recipe-app/src/App.js
@@ -1,7 +1,7 @@
 
 import { useState } from "react";
 import "./App.css";
-
+import  axios  from "axios";
 import RecipeCard from "./RecipeCard";
 export default App;
 
@@ -9,7 +9,7 @@ export default App;
 function App() {
   const [query, setquery] = useState("");
   const [recipes, setrecipes] = useState([]);
-  const [healthLabel, sethealthLabel] = useState(vegan);
+  const [healthLabel, sethealthLabel] = useState("");
 
   const YOUR_APP_ID = `ca8a7842`;
   const YOUR_APP_KEY = "8467fdd6590ea00b728798aacaf54866";
@@ -17,25 +17,25 @@ function App() {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
 
   const getRecipeInfo = async () => {
-    var result = await Axios.get(url);
+    var result = await axios.get(url);
     setrecipes(result.data.hits);
     console.log(result.data.hits);
   };
   //                  **********************    'search' removed from btwn + +
-  async () => {
-    var result = await Axios.get();
-    setrecipes(result.data.hits);
-    console.log(result.data);
+  // async () => {
+  //   var result = await Axios.get();
+  //   setrecipes(result.data.hits);
+  //   console.log(result.data);
   // }
 //  ******changing getRecipes to search was an idea...nothing happened
   const onSubmit = (e) => {
     e.preventDefault();
     setrecipes();
-  
+  }
   return (
     <div className ="app">
       <h1 > Ambar's Recipe Search 🍽️ </h1>
-      /* ***searchbar next to txt */
+     
       <form className ="search_Bar" onSubmit={onSubmit}>
         <input
           type="text"
@@ -44,23 +44,23 @@ function App() {
           value={query}
           onChange={(e) => setquery(e.target.value)}
         />
-        <input className="search_Submit" type="submit" value="Search" />
+        <input onClick={getRecipeInfo} className="search_Submit" type="submit" value="Search" />
 
         <select className="app_healthlabels">
-          <option onClick={() => sethealthLabel(vegan)}>Vegan</option>
-          <option onClick={() => sethealthLabel(vegetarian)}>Vegetarian</option>
-          <option onClick={() => sethealthLabel(pescetarian)}>pescetarian</option>
-          <option onClick={() => sethealthLabel(high-protein)}>High Protein</option>
-          <option onClick={() => sethealthLabel(low-sodium)}>low sodium</option>
+          <option onClick={() => sethealthLabel("vegan")}>Vegan</option>
+          <option onClick={() => sethealthLabel("vegetarian")}>Vegetarian</option>
+          <option onClick={() => sethealthLabel("pescetarian")}>pescetarian</option>
+          <option onClick={() => sethealthLabel("high-protein")}>High Protein</option>
+          <option onClick={() => sethealthLabel("low-sodium")}>low sodium</option>
         </select>
       </form>
       <div className="app_recipes">
-        {recipes.map((recipe) => {
-          return <RecipeCard recipe={recipe} />;
-        })}
+        {recipes ? recipes.map((recipe, i) => {
+          return <RecipeCard key={i} recipe={recipe} />;
+        }) : "no recipes"}
       </div>
     
     </div>
   
 );
-      }}}
+      }
--- HW/Wk10/recipe-app/src/RecipeCard.js
+++ HW/Wk10/recipe-app/src/RecipeCard.js
@@ -6,7 +6,8 @@ export default function RecipeCard({recipe}) {
     // .match(etc) search for these types of imgs if not then rtrn null
     recipe["recipe"]["image"].match(/|.(jpeg|jpg|gif|png)$/)
     != null && (
-     <div className='recipeCard'>
+     <div className='recipeCard' onClick={() =>
+     window.open(recipe["recipe"]["url"]) }>
     <img className='recipeCard_img' src={recipe["recipe"]["image"]} />
         <p className='recipeCard_name'>{recipe["recipe"]["label"]}</p>
     </div>
